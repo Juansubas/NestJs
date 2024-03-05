f8a27237d0b774f57f2f914d6309557048a8da28
juansubas <juansubas@gmail.com>
2024-03-05 00:30:58 -0500

    CRUD OPERATIONS IN SERVICE

--- nestjs-prisma-crud/src/task/task.controller.ts
+++ nestjs-prisma-crud/src/task/task.controller.ts
@@ -1,4 +1,8 @@
 import { Controller } from "@nestjs/common";
+import { TaskService } from "./task.service";
 
 @Controller()
-export class TaskController{}
+export class TaskController{
+
+    constructor(private readonly tasksService: TaskService) {}
+}
--- nestjs-prisma-crud/src/task/task.service.ts
+++ nestjs-prisma-crud/src/task/task.service.ts
@@ -12,5 +12,34 @@ export class TaskService{
         return this.prisma.task.findMany();
     }
 
-    
+    async getTaskById(id: number) : Promise<Task> {
+        return this.prisma.task.findUnique({
+            where: {
+                id
+            }
+        });
+    }
+
+    async createTask(data: Task) : Promise<Task> {
+        return this.prisma.task.create({
+            data
+        });
+    }
+
+    async updateTask(id: number, data: Task) : Promise<Task> {
+        return this.prisma.task.update({
+            where: {
+                id
+            },
+            data
+        });
+    }
+
+    async deleteTask(id: number) : Promise<Task> {
+        return this.prisma.task.delete({
+            where: {
+                id
+            }
+        });
+    }
 }
