d8a661a7553d14b61e5587edad5948427bcf59db
juansubas <juansubas@gmail.com>
2024-03-05 00:40:17 -0500

    Controller CRUD

--- nestjs-prisma-crud/src/task/task.controller.ts
+++ nestjs-prisma-crud/src/task/task.controller.ts
@@ -1,8 +1,34 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Post, Put, Delete, Body, Param } from "@nestjs/common";
 import { TaskService } from "./task.service";
+import { Task } from "@prisma/client";
 
 @Controller()
 export class TaskController{
 
     constructor(private readonly tasksService: TaskService) {}
+
+    @Get()
+    async getAllTask(){
+        this.tasksService.getAllTasks();
+    }
+    
+    @Post()
+    async createTask(@Body() data: Task) {
+        this.tasksService.createTask(data);
+    }
+
+    @Get(':id')
+    async getTaskById(@Param('id') id: string) {
+        this.tasksService.getTaskById(Number(id));
+    }
+
+    @Delete(':id')
+    async deleteTask(@Param('id') id: string) {
+        this.tasksService.deleteTask(Number(id));
+    }
+
+    @Put(':id')
+    async updateTask(@Param('id') id: string, @Body() data: Task){
+        this.tasksService.updateTask(Number(id), data);
+    }
 }
